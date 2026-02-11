cb69637093c3e172b35bf7b3d9923a334f52df92
Todd <todd@peoplesvp.com>
2026-02-10 21:45:04 -0600

    [TRACKING] Install Plausible custom tracking script for WhichHealthShare

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,11 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        {/* Plausible Analytics */}
-        <script defer data-domain="whichhealthshare.com" src="https://plausible.io/js/script.js"></script>
+        {/* Privacy-friendly analytics by Plausible */}
+        <script async src="https://plausible.io/js/pa-0qY3de9XxgJYC_q5t8qII.js"></script>
+        <script>
+          {`window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}}; plausible.init()`}
+        </script>
       </head>
       <body>
         <Header />
